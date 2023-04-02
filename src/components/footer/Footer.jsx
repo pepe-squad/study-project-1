95f8fe58079000e9d91c29b11da710eb9fc813e4
1lich <holykim@mail.ru>
2023-04-03 01:58:04 +0300

    adding footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <header className="flex flex-col p-4 bg-slate-800">
+      <div className="flex justify-center font-semibold text-slate-200">Footer for van</div>
+    </header>
+  );
+};
+
+export default Footer;
