e6017f39a5b781afcd91d632c81aedb79c6c3632
shut56 <kuvini@gmail.com>
2023-04-24 22:39:45 +0500

    Add router

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 
 import Head from '_components/head/Head';
 import Header from '_components/header/Header';
-import Form from '_components/form/Form';
 import Footer from '_components/footer/Footer';
 
-const Main = () => {
+const Layout = ({ children, title }) => {
   return (
     <>
-      <Head title="Main" />
+      <Head title={title} />
       <Header />
-      <Form />
+      {children}
       <Footer />
     </>
   );
 };
 
-export default Main;
+export default Layout;
