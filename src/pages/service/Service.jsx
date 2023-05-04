d6ee772b396ca692abe657c85ce96bc7cda6642c
Oris56 <seytzanov.miras@gmail.com>
2023-05-04 15:58:20 +0500

    Form Router

---
+++ src/pages/service/Service.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '_components/layout/Layout';
+
+const Service = () => {
+  return (
+    <Layout title="Contacts">
+      <div>We Offer The Best Quality Dental Services</div>
+    </Layout>
+  );
+};
+
+export default Service;
