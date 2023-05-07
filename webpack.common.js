a9223067ca80ac034f8256fa96d602eb032b956c
1lich <holykim@mail.ru>
2023-05-07 12:52:51 +0300

    added loader to common

--- webpack.common.js
+++ webpack.common.js
@@ -12,71 +12,79 @@ dotenv.config();
 const { SOCKETS_ENABLE } = process.env;
 
 const setAlias = (aliasList) => {
-    return Object.entries(aliasList).reduce((acc, [key, value]) => {
-        return {
-            ...acc,
-            [key]: resolve(process.cwd(), value)
-        };
-    }, {});
+  return Object.entries(aliasList).reduce((acc, [key, value]) => {
+    return {
+      ...acc,
+      [key]: resolve(process.cwd(), value)
+    };
+  }, {});
 };
 
 const config = {
-    entry: './src/bootstrap.jsx',
-    devtool: 'source-map',
-    resolve: {
-        alias: setAlias(alias),
-        extensions: ['.js', '.jsx', '.json']
-    },
-    output: {
-        filename: 'assets/js/[name].bundle.js',
-        path: resolve(process.cwd(), 'dist'),
-        publicPath: 'auto'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.(sa|sc|c)ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false,
-                            importLoaders: 1,
-                            sourceMap: true
-                        }
-                    },
-                    'postcss-loader',
-                    'sass-loader'
-                ]
+  entry: './src/bootstrap.jsx',
+  devtool: 'source-map',
+  resolve: {
+    alias: setAlias(alias),
+    extensions: ['.js', '.jsx', '.json']
+  },
+  output: {
+    filename: 'assets/js/[name].bundle.js',
+    path: resolve(process.cwd(), 'dist'),
+    publicPath: 'auto'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              importLoaders: 1,
+              sourceMap: true
             }
+          },
+          'postcss-loader',
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new ESLintPlugin({
-            extensions: ['js', 'jsx'],
-            exclude: 'node_modules'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/css/style.css'
-        }),
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-        new webpack.DefinePlugin({
-            SOCKETS_ENABLE: SOCKETS_ENABLE === 'true'
-        })
+      },
+      {
+        test: /\.(jpg|JPG|jpeg|png|gif|mp3|svg|ttf|woff2|woff|eot)$/gi,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
     ]
+  },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['js', 'jsx'],
+      exclude: 'node_modules'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/style.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      SOCKETS_ENABLE: SOCKETS_ENABLE === 'true'
+    })
+  ]
 };
 
 export default config;
