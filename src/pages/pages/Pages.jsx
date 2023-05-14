27a5097b01ec734e2cd3622a79aab660e5cd44b0
shut56 <kuvini@gmail.com>
2023-05-14 17:34:29 +0500

    Fix some issues

--- src/pages/pages/Pages.jsx
+++ src/pages/pages/Pages.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import Layout from '_components/layout/Layout';
-import { Link } from 'react-router-dom';
 
 const Pages = () => {
   const [docList, setDocList] = useState([]);
