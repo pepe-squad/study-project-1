3026a4779ebb8e76327aa12cb869b29441ce8991
1lich <56218879+1lich@users.noreply.github.com>
2023-05-15 19:00:00 +0300

    Feature/about (#12)
    * added footer with some content
    
    * made some fixes
    
    * corrections according to comments
    
    * corrections according to comments
    
    * change naming
    
    * bugfix
    
    * change about,  footer and header
    
    * change about,  footer and header2
    
    * footer and header fix position
    
    * add shadow to header
    
    * set space between text
    
    * change PORT, add file loader and import pic
    
    * fixed loader
    
    * fixed loader
    
    * fixed loader
    
    * fixed loader
    
    * fixed loader
    
    * fixed loader
    
    * added loader to common
    
    * added loader to common
    
    * final fix of cfg
    
    * added env variables and fixed contact page
    
    * added variables for style class
    
    * modify env.example

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,12 +2,51 @@ import React from 'react';
 
 import Layout from '_components/layout/Layout';
 
+import pic from './about.jpg';
+
+import Appointment from './components/Appontment';
+
 const About = () => {
   return (
-    <Layout title="About">
-      <h1 className="text-2xl">About</h1>
-      <div>Hello! This is About page!</div>
-      <div>Have a nice day! :)</div>
+    <Layout title="About Us">
+      <div className="flex items-center justify-center">
+        <div className="grid grid-rows-1 w-3/5">
+          <div className="grid grid-cols-3">
+            <div className="font-serif col-span-2">
+              <h5>About Us</h5>
+              <h1 className="inline-block text-6xl my-3  font-bold">
+                The World&apos;s Best Dental Clinic That You Can Trust
+              </h1>
+              <h4 className="inline-block text-2xl italic font-bold text-gray-500 my-3">
+                Diam dolor diam ipsum sit. Clita erat ipsum et lorem stet no lorem sit clita duo justo magna dolore
+              </h4>
+              <p className="inline-block text-xl text-gray-500 my-3">
+                Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet diam et eos
+                labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et
+                tempor eirmod magna dolore erat amet
+              </p>
+              <div className="grid grid-rows-1 text-2xl my-3">
+                <div className="grid grid-cols-2">
+                  <div>
+                    <h5>Award winning</h5>
+                    <h5>Professional staff</h5>
+                  </div>
+                  <div>
+                    <h5>24/7 Opened</h5>
+                    <h5>Fair prices</h5>
+                  </div>
+                  <div>
+                    <Appointment />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <img src={pic} alt="Nothing to depict" />
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
