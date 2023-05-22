3b142e1b64f01142e36306389ba5424afc4e5385
1lich <holykim@mail.ru>
2023-05-22 21:58:59 +0300

    add profiles

--- src/pages/pages/staff/Staff.jsx
+++ src/pages/pages/staff/Staff.jsx
@@ -1,26 +1,50 @@
 import React, { useEffect, useState } from 'react';
 
 import Layout from '_components/layout/Layout';
+import { Link } from 'react-router-dom';
 
 const Staff = () => {
   const [docList, setDocList] = useState([]);
+  const specs = [
+    'Dentist-therapist',
+    'Orthodontist',
+    'Orthopedist',
+    'Periodontist',
+    'Surgeon',
+    "Children's dentist",
+    'Hygienist',
+    'Dentist',
+    'Technician',
+    'Dental assistant'
+  ];
 
   useEffect(() => {
     fetch('https://dummyjson.com/users')
       .then((res) => res.json())
-      .then(({ users }) => setDocList(users));
+      .then(({ users }) => {
+        users.splice(20, 10);
+        setDocList(users);
+      });
   }, []);
+
   return (
     <Layout title="Staff">
-      {docList.map((doc) => {
-        const fullName = `${doc.firstName} ${doc.lastName}`;
-        return (
-          <div key={doc.id}>
-            <div>{fullName}</div>
-            <img src={doc.image} alt={fullName} />
-          </div>
-        );
-      })}
+      <div className="flex flex-wrap items-center  justify-around mx-10">
+        {docList.map((doc) => {
+          const fullName = `${doc.firstName} ${doc.lastName}`;
+          return (
+            <Link key={doc.id} to={`${doc.id}`} className="flex flex-col basis-1/12 m-4">
+              <div>
+                <div className="font-serif text-xl font-bold text-center">{fullName}</div>
+                <img src={doc.image} alt={fullName} />
+                <div className="font-serif text-center">
+                  {`Specialization: ${specs[Math.floor(Math.random() * 10)]}`}
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </Layout>
   );
 };
--- src/pages/pages/staff/profile/Profile.jsx
+++ src/pages/pages/staff/profile/Profile.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Layout from '_components/layout/Layout';
 
 const Profile = () => {
-  return <Layout title="Profile">Profile</Layout>;
+  const { id } = useParams();
+  const [docProfile, setDocProfile] = useState({});
+  const fullName = `${docProfile.firstName} ${docProfile.lastName}`;
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/users/${id}`)
+      .then((res) => res.json())
+      .then((profile) => {
+        setDocProfile(profile);
+      });
+  }, [id]);
+
+  return (
+    <Layout title="Profile" className="content-center">
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div>
+          <img src={docProfile.image} alt="No pics" />
+        </div>
+        <div>{fullName}</div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Profile;
