bff77f9c6c956785f49c1854390d4b915e082904
1lich <holykim@mail.ru>
2023-05-23 14:30:15 +0300

    add style for card

--- src/pages/pages/staff/profile/Profile.jsx
+++ src/pages/pages/staff/profile/Profile.jsx
@@ -17,12 +17,15 @@ const Profile = () => {
   }, [id]);
 
   return (
-    <Layout title="Profile" className="content-center">
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <div>
-          <img src={docProfile.image} alt="No pics" />
+    <Layout title="Profile">
+      <div className="m-auto max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <img className="rounded-t-lg" src={docProfile.image} alt="No pics" />
+
+        <div className="p-5 text-center bg-cyan-50">
+          <h5 className="mb-2 text-2xl font-bold  tracking-tight text-gray-900 dark:text-white">{fullName}</h5>
+
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">THIS IS DENTIST</p>
         </div>
-        <div>{fullName}</div>
       </div>
     </Layout>
   );
