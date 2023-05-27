546d0a3656d02ea643c55f4cae0c0fde04c02a51
shut56 <kuvini@gmail.com>
2023-05-27 15:46:29 +0500

    Change grid to flex

--- src/pages/pages/staff/Staff.jsx
+++ src/pages/pages/staff/Staff.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import Layout from '_components/layout/Layout';
-import { Link } from 'react-router-dom';
+
+const specs = [
+  'Dentist-therapist',
+  'Orthodontist',
+  'Orthopedist',
+  'Periodontist',
+  'Surgeon',
+  "Children's dentist",
+  'Hygienist',
+  'Dentist',
+  'Technician',
+  'Dental assistant'
+];
 
 const Staff = () => {
   const [docList, setDocList] = useState([]);
-  const specs = [
-    'Dentist-therapist',
-    'Orthodontist',
-    'Orthopedist',
-    'Periodontist',
-    'Surgeon',
-    "Children's dentist",
-    'Hygienist',
-    'Dentist',
-    'Technician',
-    'Dental assistant'
-  ];
 
   useEffect(() => {
     fetch('https://dummyjson.com/users')
@@ -29,12 +30,12 @@ const Staff = () => {
 
   return (
     <Layout title="Staff">
-      <div className="grid grid-cols-4 gap-4 mx-80">
+      <div className="flex flex-wrap p-12 gap-4">
         {docList.map((doc) => {
           const fullName = `${doc.firstName} ${doc.lastName}`;
           return (
             <Link key={doc.id} to={`${doc.id}`}>
-              <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="grow w-48 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <img className="w-28 h-28 mx-auto" src={doc.image} alt="assets/images/doc-male.png" />
                 <div className="bg-cyan-50 text-center">
                   <h5 className="mt-8  text-xl font-medium text-slate-950">{fullName}</h5>
