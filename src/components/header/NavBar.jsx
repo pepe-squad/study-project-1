278645b246b88652a291b261ad99d5c943c347e1
shut56 <kuvini@gmail.com>
2023-05-27 16:31:24 +0500

    Update NavBar styles

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-  const linkStyleClass = 'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
   return (
     <div>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+      <nav className="nav">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="hidden w-full md:block md:w-auto">
             <div className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <Link to="/" className={linkStyleClass}>
+              <Link to="/" className="navLink">
                 Home
               </Link>
-              <Link to="/about" className={linkStyleClass}>
+              <Link to="/about" className="navLink">
                 About us
               </Link>
-              <Link to="/contact" className={linkStyleClass}>
+              <Link to="/contact" className="navLink">
                 Contact
               </Link>
-              <Link to="/service" className={linkStyleClass}>
+              <Link to="/service" className="navLink">
                 Service
               </Link>
             </div>
