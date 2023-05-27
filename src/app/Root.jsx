06e3ffc4573fac6358e1d6dd9eea32af88896d62
miras <miras@LinuxMiras.myguest.virtualbox.org>
2023-05-27 15:11:43 +0500

    Contacts

--- src/app/Root.jsx
+++ src/app/Root.jsx
@@ -5,6 +5,7 @@ import Main from '_pages/main/Main';
 import About from '_pages/about/About';
 import Service from '_pages/service/Service';
 import Contact from '_pages/contact/Contact';
+import Branches from '_pages/contact/branches/Branches';
 
 import Startup from './Startup';
 
@@ -17,6 +18,7 @@ const Root = () => {
           <Route path="about" element={<About />} />
           <Route path="service" element={<Service />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="contact/:id" element={<Branches />} />
           <Route path="*" element={<div>404</div>} />
         </Routes>
       </BrowserRouter>
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import Layout from '_components/layout/Layout';
 
 const Contact = () => {
+  const [branches, setBranches] = useState([]);
+  useEffect(() => {
+    setBranches([
+      {
+        id: 0,
+        name: 'Main Office',
+        city: 'Washington',
+        email: 'info@example.com',
+        phone: '+012 345 6789',
+      },
+      {
+        id: 1,
+        name: 'East Office',
+        city: 'New-York',
+        email: 'east@example.com',
+        phone: '+012 345 6790',
+      },
+      {
+        id: 2,
+        name: 'West Office',
+        city: 'Seatle',
+        email: 'west@example.com',
+        phone: '+012 345 6791',
+      },
+    ]);
+  }, []);
+  const mainOffice = branches.filter((item) => {
+    return item.city === 'Washington';
+  });
+  const eastOffice = branches.filter((item) => {
+    return item.city === 'New-York';
+  });
+  const westOffice = branches.filter((item) => {
+    return item.city === 'Seatle';
+  });
   return (
     <Layout title="Contact">
-      <h1 className="text-2xl">Contact</h1>
-      <div>Hello! This is Contact page!</div>
-      <div>Have a nice day! :</div>
+      <div className="flex flex-row grid grid-cols-3 gap-20 items-stretch pl-20 py-5">
+        {mainOffice.map((item) => {
+          return (
+            <Link key={item.id} to={`${item.id}`}>
+              <div>{item.name}</div>
+              <div>{item.email}</div>
+              <div>{item.phone}</div>
+              <div>{item.city}</div>
+            </Link>
+          )
+        })}
+        {eastOffice.map((item) => {
+          return (
+            <Link key={item.id} to={`${item.id}`}>
+              <div>{item.name}</div>
+              <div>{item.email}</div>
+              <div>{item.phone}</div>
+              <div>{item.city}</div>
+            </Link>
+          )
+        })}
+        {westOffice.map((item) => {
+          return (
+            <Link key={item.id} to={`${item.id}`}>
+              <div>{item.name}</div>
+              <div>{item.email}</div>
+              <div>{item.phone}</div>
+              <div>{item.city}</div>
+            </Link>
+          )
+        })}
+      </div>
     </Layout>
   );
 };
