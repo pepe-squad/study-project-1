2f2aae9999fd8d4dfd277a84b75f0d4b8d6c6687
shut56 <kuvini@gmail.com>
2023-05-27 16:07:56 +0500

    Update contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,74 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Layout from '_components/layout/Layout';
 
+const branches = [
+  {
+    id: 0,
+    name: 'Main Office',
+    city: 'Washington',
+    email: 'info@example.com',
+    phone: '+012 345 6789'
+  },
+  {
+    id: 1,
+    name: 'East Office',
+    city: 'New-York',
+    email: 'east@example.com',
+    phone: '+012 345 6790'
+  },
+  {
+    id: 2,
+    name: 'West Office',
+    city: 'Seatle',
+    email: 'west@example.com',
+    phone: '+012 345 6791'
+  },
+  {
+    id: 3,
+    name: 'West Office',
+    city: 'Washington',
+    email: 'west@example.com',
+    phone: '+012 345 6791'
+  }
+];
+
 const Contact = () => {
-  const [branches, setBranches] = useState([]);
-  useEffect(() => {
-    setBranches([
-      {
-        id: 0,
-        name: 'Main Office',
-        city: 'Washington',
-        email: 'info@example.com',
-        phone: '+012 345 6789'
-      },
-      {
-        id: 1,
-        name: 'East Office',
-        city: 'New-York',
-        email: 'east@example.com',
-        phone: '+012 345 6790'
-      },
-      {
-        id: 2,
-        name: 'West Office',
-        city: 'Seatle',
-        email: 'west@example.com',
-        phone: '+012 345 6791'
-      }
-    ]);
-  }, []);
-  const mainOffice = branches.filter((item) => {
-    return item.city === 'Washington';
-  });
-  const eastOffice = branches.filter((item) => {
-    return item.city === 'New-York';
-  });
-  const westOffice = branches.filter((item) => {
-    return item.city === 'Seatle';
-  });
   return (
     <Layout title="Contact">
-      <div className="flex flex-row grid grid-cols-3 gap-20 items-stretch pl-20 py-5">
-        {mainOffice.map((item) => {
+      <div className="flex flex-row gap-20 items-stretch pl-20 py-5">
+        {branches.map((item) => {
           return (
             <Link key={item.id} to={`${item.id}`}>
-              <div>{item.name}</div>
-              <div>{item.email}</div>
-              <div>{item.phone}</div>
-              <div>{item.city}</div>
-            </Link>
-          );
-        })}
-        {eastOffice.map((item) => {
-          return (
-            <Link key={item.id} to={`${item.id}`}>
-              <div>{item.name}</div>
-              <div>{item.email}</div>
-              <div>{item.phone}</div>
-              <div>{item.city}</div>
-            </Link>
-          );
-        })}
-        {westOffice.map((item) => {
-          return (
-            <Link key={item.id} to={`${item.id}`}>
-              <div>{item.name}</div>
-              <div>{item.email}</div>
-              <div>{item.phone}</div>
-              <div>{item.city}</div>
+              <div>
+                <div>{item.name}</div>
+                <div>{item.email}</div>
+                <div>{item.phone}</div>
+                <div>{item.city}</div>
+              </div>
             </Link>
           );
         })}
