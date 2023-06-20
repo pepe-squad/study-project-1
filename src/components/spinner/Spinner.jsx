ee65246becf1bf4efce6bbea71ed37ee1e174aa8
1lich <holykim@mail.ru>
2023-06-20 17:06:53 +0300

    add spinner

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './style.scss';
+
+const Spinner = () => {
+  return (
+    <div className="lds-spinner">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default Spinner;
--- src/pages/pages/staff/Staff.jsx
+++ src/pages/pages/staff/Staff.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useStore } from 'effector-react';
 
 import Layout from '_components/layout/Layout';
+import Spinner from '_components/spinner/Spinner';
 import { docListStore, getDocListEvent } from '_services/commonDomain';
 
 const Staff = () => {
@@ -14,24 +15,29 @@ const Staff = () => {
 
   return (
     <Layout title="Staff">
-      <div className="flex flex-wrap gap-4 justify-between p-12">
-        {docList.map((doc) => {
-          const fullName = `${doc.firstName} ${doc.lastName}`;
-          return (
-            <Link key={doc.id} to={`${doc.id}`}>
-              <div className="staffCards">
-                <img className="w-28 h-28 mx-auto" src={doc.image} alt="assets/images/doc-male.png" />
-                <div className="bg-cyan-50 text-center">
-                  <h5 className="mt-8  text-xl font-medium text-slate-950">{fullName}</h5>
-                  <span className="text-sm text-blue-500">
-                    {/* {`Specialization: ${specs[Math.floor(Math.random() * 10)]}`} */}
-                  </span>
+      {' '}
+      {!docList ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-wrap gap-4 justify-between p-12">
+          {docList.map((doc) => {
+            const fullName = `${doc.firstName} ${doc.lastName}`;
+            return (
+              <Link key={doc.id} to={`${doc.id}`}>
+                <div className="staffCards">
+                  <img className="w-28 h-28 mx-auto" src={doc.image} alt="assets/images/doc-male.png" />
+                  <div className="bg-cyan-50 text-center">
+                    <h5 className="mt-8  text-xl font-medium text-slate-950">{fullName}</h5>
+                    <span className="text-sm text-blue-500">
+                      {/* {`Specialization: ${specs[Math.floor(Math.random() * 10)]}`} */}
+                    </span>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </Layout>
   );
 };
