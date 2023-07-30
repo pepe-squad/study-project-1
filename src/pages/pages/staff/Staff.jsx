20cc24dc055fed9987d14e0534e5d14d7aa56b6b
1lich <holykim@mail.ru>
2023-07-30 18:38:47 +0300

    styled staff

--- src/pages/pages/staff/Staff.jsx
+++ src/pages/pages/staff/Staff.jsx
@@ -5,6 +5,22 @@ import { useStore } from 'effector-react';
 import Layout from '_components/layout/Layout';
 import Spinner from '_components/spinner/Spinner';
 import { docListStore, getDocListEvent, getDocListStatusStore } from '_services/commonDomain';
+import {
+  BottomDiv,
+  ProfileContainerDiv,
+  ProfileImage,
+  ProfileImageDiv,
+  ProfileLinksA,
+  ProfileLinksDiv,
+  ProfileNameDiv,
+  ProfileNameH4,
+  ProfileSpec,
+  StaffGridDiv,
+  StyledInstaIcon,
+  StyledTiktokIcon,
+  StyledTwitterIcon,
+  StyledVkIcon
+} from './styled-staff';
 
 const Staff = () => {
   const docList = useStore(docListStore);
@@ -19,22 +35,40 @@ const Staff = () => {
       {isPending ? (
         <Spinner />
       ) : (
-        <div className="flex flex-wrap gap-4 justify-between p-12">
-          {docList.map((doc) => {
-            const fullName = `${doc.firstName} ${doc.lastName}`;
-            return (
-              <Link key={doc.id} to={`${doc.id}`}>
-                <div className="staffCards">
-                  <img className="w-28 h-28 mx-auto" src={doc.image} alt="assets/images/doc-male.png" />
-                  <div className="bg-cyan-50 text-center">
-                    <h5 className="mt-8  text-xl font-medium text-slate-950">{fullName}</h5>
-                    <span className="text-sm text-blue-500">{`Specialization: ${doc.spec}`}</span>
-                  </div>
-                </div>
-              </Link>
-            );
-          })}
-        </div>
+        <BottomDiv>
+          <StaffGridDiv>
+            {docList.map((doc) => {
+              const fullName = `${doc.firstName} ${doc.lastName}`;
+              return (
+                <Link key={doc.id} to={`${doc.id}`}>
+                  <ProfileContainerDiv>
+                    <ProfileImageDiv>
+                      <ProfileImage src={doc.image} />
+                      <ProfileLinksDiv>
+                        <ProfileLinksA>
+                          <StyledTwitterIcon />
+                        </ProfileLinksA>
+                        <ProfileLinksA>
+                          <StyledInstaIcon />
+                        </ProfileLinksA>
+                        <ProfileLinksA>
+                          <StyledVkIcon />
+                        </ProfileLinksA>
+                        <ProfileLinksA>
+                          <StyledTiktokIcon />
+                        </ProfileLinksA>
+                      </ProfileLinksDiv>
+                    </ProfileImageDiv>
+                    <ProfileNameDiv>
+                      <ProfileNameH4>{fullName}</ProfileNameH4>
+                      <ProfileSpec>{doc.spec}</ProfileSpec>
+                    </ProfileNameDiv>
+                  </ProfileContainerDiv>
+                </Link>
+              );
+            })}
+          </StaffGridDiv>
+        </BottomDiv>
       )}
     </Layout>
   );
---
+++ src/pages/pages/staff/styled-staff.jsx
@@ -0,0 +1,116 @@
+import styled from 'styled-components';
+import { FaTwitter, FaInstagram, FaVk, FaTiktok } from 'react-icons/fa';
+
+export const CommonDiv = styled.div``;
+
+export const TopDiv = styled.div``;
+
+export const BottomDiv = styled.div`
+  box-sizing: border-box;
+  display: block;
+`;
+
+export const StaffGridDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const ProfileContainerDiv = styled.div`
+  display: block;
+  box-sizing: border-box;
+  flex-shrink: 1;
+  padding: 0 2rem;
+  margin-top: 1.5rem;
+`;
+
+export const ProfileImageDiv = styled.div`
+  z-index: -1;
+`;
+
+export const ProfileImage = styled.img`
+  z-index: 1;
+  box-sizing: border-box;
+  vertical-align: middle;
+  overflow-clip-margin: content-box;
+  overflow: clip;
+`;
+
+export const ProfileLinksDiv = styled.div`
+  background-color: #eef9ff;
+  z-index: 2;
+  display: flex;
+  padding: 0.5rem;
+  margin-left: 17%;
+  margin-top: -10%;
+  margin-bottom: -10%;
+  width: 65%;
+  box-sizing: border-box;
+`;
+
+export const StyledTwitterIcon = styled(FaTwitter)`
+  background-color: #06a3da;
+  color: white;
+  font-size: 24px;
+  width: 36px;
+  height: 36px;
+`;
+
+export const StyledInstaIcon = styled(FaInstagram)`
+  background-color: #06a3da;
+  color: white;
+  font-size: 24px;
+  width: 36px;
+  height: 36px;
+`;
+
+export const StyledVkIcon = styled(FaVk)`
+  background-color: #06a3da;
+  color: white;
+  font-size: 24px;
+  width: 36px;
+  height: 36px;
+`;
+
+export const StyledTiktokIcon = styled(FaTiktok)`
+  background-color: #06a3da;
+  color: white;
+  font-size: 24px;
+  width: 36px;
+  height: 36px;
+`;
+
+export const ProfileLinksA = styled.a`
+  padding-left: 0;
+  padding-right: 0;
+  text-align: center;
+  width: 36px;
+  height: 36px;
+  color: #ffffff;
+  margin: 0.25rem;
+  background-color: #06a3da;
+`;
+
+export const ProfileNameDiv = styled.div`
+  background-color: #eef9ff;
+  padding: 2rem;
+`;
+
+export const ProfileNameH4 = styled.h4`
+  font-family: 'Jost', sans-serif;
+  font-size: 1.5rem;
+  line-height: 1.2;
+  font-weight: 600;
+  color: #091e3e;
+  text-align: center;
+`;
+
+export const ProfileSpec = styled.p`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #06a3da;
+  text-align: center;
+  margin-top: 0.5rem;
+`;
