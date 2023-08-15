29e9eb0f0f4f0f60f60be5e1c3e79f7583989b8e
1lich <holykim@mail.ru>
2023-08-15 21:41:49 +0300

    styled header top

--- src/components/header/components/appointment/Appointment.jsx
+++ src/components/header/components/appointment/Appointment.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 import StyledAppointmentButton from './styled-appointment';
 
 const Appointment = () => {
-  return <StyledAppointmentButton>Appointment</StyledAppointmentButton>;
+  const ololo = () => {
+    alert('Filling is 300 bucks');
+  };
+  return <StyledAppointmentButton onClick={ololo}>Appointment</StyledAppointmentButton>;
 };
 
 export default Appointment;
